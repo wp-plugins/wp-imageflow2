71734231222623b1b40e3de0a78200c43cffcaa9
BStofko <BStofko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-07 17:45:51 +0000

    Version 1.2.5
    git-svn-id: https://plugins.svn.wordpress.org/wp-imageflow2/trunk@214568 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/imageflow.js
+++ js/imageflow.js
@@ -501,6 +501,65 @@ document.onkeydown = function(event)
 	}
 }
 
+function getPageScroll(){
+	var xScroll, yScroll;
+	if (self.pageYOffset) {
+		yScroll = self.pageYOffset;
+		xScroll = self.pageXOffset;
+	} else if (document.documentElement && document.documentElement.scrollTop){	 // Explorer 6 Strict
+		yScroll = document.documentElement.scrollTop;
+		xScroll = document.documentElement.scrollLeft;
+	} else if (document.body) {// all other Explorers
+		yScroll = document.body.scrollTop;
+		xScroll = document.body.scrollLeft;	
+	}
+	arrayPageScroll = new Array(xScroll,yScroll) 
+	return arrayPageScroll;
+}
+
+function getPageSize(){
+	var xScroll, yScroll;
+	if (window.innerHeight && window.scrollMaxY) {	
+		xScroll = window.innerWidth + window.scrollMaxX;
+		yScroll = window.innerHeight + window.scrollMaxY;
+	} else if (document.body.scrollHeight > document.body.offsetHeight){ // all but Explorer Mac
+		xScroll = document.body.scrollWidth;
+		yScroll = document.body.scrollHeight;
+	} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
+		xScroll = document.body.offsetWidth;
+		yScroll = document.body.offsetHeight;
+	}
+	var windowWidth, windowHeight;
+	if (self.innerHeight) {	// all except Explorer
+		if(document.documentElement.clientWidth){
+			windowWidth = document.documentElement.clientWidth; 
+		} else {
+			windowWidth = self.innerWidth;
+		}
+		windowHeight = self.innerHeight;
+	} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+		windowWidth = document.documentElement.clientWidth;
+		windowHeight = document.documentElement.clientHeight;
+	} else if (document.body) { // other Explorers
+		windowWidth = document.body.clientWidth;
+		windowHeight = document.body.clientHeight;
+	}
+	// for small pages with total height less then height of the viewport
+	if(yScroll < windowHeight){
+		pageHeight = windowHeight;
+	} else { 
+		pageHeight = yScroll;
+	}
+	// for small pages with total width less then width of the viewport
+	if(xScroll < windowWidth){	
+		pageWidth = xScroll;		
+	} else {
+		pageWidth = windowWidth;
+	}
+	arrayPageSize = new Array(pageWidth,pageHeight,windowWidth,windowHeight) 
+	return arrayPageSize;
+}
+
 function showFlash(){
 	var flashObjects = document.getElementsByTagName("object");
 	for (i = 0; i < flashObjects.length; i++) {
@@ -533,6 +592,9 @@ function showTop(image)
 	hideFlash();
 
 	// Show the background overlay ...
+	var arrayPageSize = getPageSize();
+	overlay_div.style.width = arrayPageSize[0] + "px";
+	overlay_div.style.height = arrayPageSize[1] + "px";
 	overlay_div.style.visibility = 'visible';
 
 	// Get the top box data set up first
@@ -565,21 +627,26 @@ function showImg(image, img_width, img_height)
 	// Set the image width property
 	topboximg_div.width = boxWidth;
 
-	var screenWidth, screenHeight;
-	if (self.innerHeight) {	// all except Explorer
-		if(document.documentElement.clientWidth){
-			screenWidth = document.documentElement.clientWidth; 
-		} else {
-			screenWidth = self.innerWidth;
-		}
-		screenHeight = self.innerHeight;
-	} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
-		screenWidth = document.documentElement.clientWidth;
-		screenHeight = document.documentElement.clientHeight;
-	} else if (document.body) { // other Explorers
-		screenWidth = document.body.clientWidth;
-		screenHeight = document.body.clientHeight;
-	}
+	var arrayPageSize = getPageSize();
+	var screenWidth = arrayPageSize[2];
+	var screenHeight = arrayPageSize[3];
+
+	var arrayPageScroll = getPageScroll();
+
+	//if (self.innerHeight) {	// all except Explorer
+	//	if(document.documentElement.clientWidth){
+	//		screenWidth = document.documentElement.clientWidth; 
+	//	} else {
+	//		screenWidth = self.innerWidth;
+	//	}
+	//	screenHeight = self.innerHeight;
+	//} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
+	//	screenWidth = document.documentElement.clientWidth;
+	//	screenHeight = document.documentElement.clientHeight;
+	//} else if (document.body) { // other Explorers
+	//	screenWidth = document.body.clientWidth;
+	//	screenHeight = document.body.clientHeight;
+	//}
 
 	// scale the box if the image is larger than the screen
 	if (boxWidth > screenWidth) {
@@ -593,8 +660,8 @@ function showImg(image, img_width, img_height)
 		topboximg_div.width = boxWidth;
 	}
 
-	xPos = Math.floor((screenWidth - boxWidth) * 0.5);
-	yPos = Math.floor((screenHeight - boxHeight) * 0.5);
+	xPos = Math.floor((screenWidth - boxWidth) * 0.5) + arrayPageScroll[0];
+	yPos = Math.floor((screenHeight - boxHeight) * 0.5) + arrayPageScroll[1];
 
 	topbox_div.style.left = xPos + 'px';
 	topbox_div.style.top = yPos + 'px';
