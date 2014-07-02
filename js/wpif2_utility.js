ae22637d4deb11f1a2f3103813cd91a45df8a3aa
BStofko <BStofko@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-02 14:51:33 +0000

    Tagging 2.0.2
    git-svn-id: https://plugins.svn.wordpress.org/wp-imageflow2/tags/2.0.2@942215 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/wpif2_utility.js
@@ -0,0 +1,26 @@
+/*
+**	WP-Imageflow Plus utility scripts on admin pages
+*/
+
+// Validate Hex color code of given colorcode, background turns red if invalid
+function colorcode_validate(element, colorcode) {
+	var regColorcode = /^(#)?([0-9a-fA-F]{3})([0-9a-fA-F]{3})?$/;
+	var style2 = element.style;
+
+	if((regColorcode.test(colorcode) == false) && (colorcode != 'transparent')) {
+		style2.backgroundColor = "#CD0000";
+	} else {
+		style2.backgroundColor = "#FFFFFF";
+	}
+}
+
+function toggleVisibility(id) {
+	var e = document.getElementById(id);
+	if(e.style.display == "block") {
+		e.style.display = "none";
+	} else {
+		e.style.display = "block";
+	}
+}
+
+
